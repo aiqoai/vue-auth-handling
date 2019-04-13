8737e9b0e23f5025edec13ff888afeb2dbab6ae0
george <bochuxt7@gmail.com>
2019-04-12 18:49:41 -0700

    add http

---
+++ src/store/httpcommon.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+
+
+// const fetchClient = () => {
+  const defaultOptions = {
+    baseURL: `http://127.0.0.1:4000/`,//process.env.APP_API_PATH,
+    //method: 'get',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  // Create instance
+export const HTTP = axios.create(defaultOptions);
+
+  // Set the AUTH token for any request
+      HTTP.interceptors.request.use(function (config) {
+          const token = localStorage.getItem('token');
+          config.headers.Authorization =  token ? `Bearer ${token}` : '';
+          return config;
+        });
+
+  // return instance;
+// };
+
+
+// export default fetchClient();
