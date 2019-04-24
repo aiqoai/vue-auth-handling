5d36ec1f8c203396fe85e21e82cfb6c2f7655900
george <bochuxt7@gmail.com>
2019-04-24 14:59:19 -0700

    fix

--- nginx/ssl.sh
+++ nginx/ssl.sh
@@ -8,3 +8,7 @@ openssl req -new -key server.key -out server.csr
 openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
 
 openssl rsa -in futurestudio_with_pass.key -out futurestudio.key
+
+
+
+#https://www.humankode.com/ssl/how-to-set-up-free-ssl-certificates-from-lets-encrypt-using-docker-and-nginx
